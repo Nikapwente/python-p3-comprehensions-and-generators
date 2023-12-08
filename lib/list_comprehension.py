07e58392115e9972e757dd2a8f2bb1bedddeb054
Nikapwente <hrsmajor@gmail.com>
2023-12-08 08:40:19 +0300

    Assignment Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,10 @@
 
 def return_evens(num_list):
     pass
+    evens = [n for n in num_list if (n%2==0)]
+    return evens
 
 def make_exclamation(sentence_list):
     pass
+    exclaimed = [n + "!" for n in sentence_list]
+    return exclaimed
